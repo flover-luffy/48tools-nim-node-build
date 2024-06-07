617ce58a21d702726c2da0d39ba04fb8524a5475
Haochen Duan <haochenduan@microsoft.com>
2024-06-08 01:49:47 +0800

    ci: 修改脚本

--- ci-build.mjs
+++
@@ -1,8 +0,0 @@
-import process from 'node:process';
-import path from 'node:path';
-import { command } from './repo/scripts/helper.mjs';
-
-const repo = path.join(process.cwd(), 'repo');
-
-await command('npm', ['install'], repo);
-await command('npm', ['run', 'build'], repo);
